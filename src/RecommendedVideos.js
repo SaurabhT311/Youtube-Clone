42b7c44cf5129a5aa95fe2cd21955a52b69c6523
SaurabhT311 <tripathisaurabh.311@gmail.com>
2021-04-04 01:53:54 +0530

    [Saurabh]Add Added Header In The YouTube

---
+++ src/RecommendedVideos.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./RecommendedVideos.css";
+function RecommendedVideos() {
+    return (
+        <div className="recommended">
+            <h2>This is videos section</h2>
+        </div>
+    )
+}
+
+export default RecommendedVideos
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDahO8j9hTSFIVfgmEJnvVZwDGhJyYzdgE",
+    authDomain: "clone-92330.firebaseapp.com",
+    projectId: "clone-92330",
+    storageBucket: "clone-92330.appspot.com",
+    messagingSenderId: "787632468593",
+    appId: "1:787632468593:web:99dc29b492af6ce718e1d0",
+    measurementId: "G-0MKT4GN24H"
+  };
